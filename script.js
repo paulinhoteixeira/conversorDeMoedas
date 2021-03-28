9e4c07c97b4a705ae7bf73cb705bc18eaf3360ef
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-03-28 18:38:05 -0300

    código desenvolvido por mim para digitar e exibir os valores na própria tela

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let valorEmDolar = parseFloat(prompt("Digite o valor em dolar!"))
+// Código conforme demonstrado pelos professores da Alura
+/*let valorEmDolar = parseFloat(prompt("Digite o valor em dolar!"))
 
 let valorEmReal = (valorEmDolar * 5.50).toFixed(2)
 
 
 alert(valorEmReal)
+
+*/
+
+//Código desenvolvido por mim para digitar e exibir os valores na própira tela
+let valorDigitado = 0;
+function capturar () {
+    valorDigitado = document.getElementById("dolarDigitado").value;
+    valorDigitado = parseFloat(valorDigitado.replace(',','.'))
+    let valorEmReal = (valorDigitado * 5.50).toFixed(2).replace(".",",")
+    document.getElementById("valorEmReal").innerHTML = "R$" + valorEmReal;
+}
 
